79d531f1d5751908a5f2b04a99f5820acaa0f11c
Martin SangDo <sang.do@knorex.com>
2021-05-01 05:00:38 +0700

    - forgot password

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -2,30 +2,30 @@
 const nodemailer = require("nodemailer");
 
 // create reusable transporter object using the default SMTP transport
-// const transporter = nodemailer.createTransport({
-//     type: process.env.MAIL_DRIVER,
-//     host: process.env.MAIL_HOST,
-//     port: process.env.MAIL_PORT,
-//     secureConnection: process.env.MAIL_ENCRYPTION, // true for 465, false for other ports
-//     auth: {
-//         user: process.env.MAIL_USERNAME, // generated ethereal user
-//         pass: process.env.MAIL_PASSWORD, // generated ethereal password
-//     },
-//     tls: {
-//         rejectUnauthorized: false, //force send with unauthorized
-//     }
-// });
-
-var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    logger: true,
-    debug: true,
+const transporter = nodemailer.createTransport({
+    type: process.env.MAIL_DRIVER,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secureConnection: process.env.MAIL_ENCRYPTION, // true for 465, false for other ports
     auth: {
-      user: "mernshop3393@gmail.com",
-      pass: "mernshop@123"
+        user: process.env.MAIL_USERNAME, // generated ethereal user
+        pass: process.env.MAIL_PASSWORD, // generated ethereal password
+    },
+    tls: {
+        rejectUnauthorized: false, //force send with unauthorized
     }
-  });
+});
+
+// var transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     logger: true,
+//     debug: true,
+//     auth: {
+//       user: "mernshop3393@gmail.com",
+//       pass: "mernshop@123"
+//     }
+//   });
 
 exports.sendEmailForgotPassword = (params) => {
     const {email, token} = params
@@ -34,7 +34,7 @@ exports.sendEmailForgotPassword = (params) => {
         transporter.sendMail({
             from: process.env.FROM_EMAIL, // sender address
             to: [email, process.env.TO_EMAIL], // list of receivers
-            subject: 'Password reset link',
+            subject: 'Email thay đổi mật khẩu',
             html: `
                 <p>Please click this button to reset your password</p>
                 <a href="${process.env.CLIENT_URL}/user/reset_password/${token}">Reset password</a>
